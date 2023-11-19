0d732c01f723fc0b7f36c5652461cb7bdcbb0f96
novakvova <novakvova@gmail>
2023-11-19 16:22:25 +0200

    Working Drop apple

--- ApplePicker/Assets/Scripts/AppleTree.cs
+++ ApplePicker/Assets/Scripts/AppleTree.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //ќб'Їкт, €кий буде вил≥тать з €блун≥
+    public GameObject applePrefab;
+    //швидк≥сть перем≥щенн€
+    public float speed = 1f;
+    //меж≥ перем≥щенн€ €блун≥
+    public float leftAndRightEdge = 10f;
+    //≤мов≥рн≥сть зм≥ни напр€ку руху
+    public float chanceToChangeDirections = 0.1f;
+    //ск≥льк≥ раз≥в у секунду буде кидать €блука
+    public float secondsBetweenAppleDrops = 1f;
+
+    // «апускаЇмо процес скиданн€ €блук
     void Start()
     {
-        
+        Invoke("DropApple", 2);
     }
 
-    // Update is called once per frame
+    private void DropApple()
+    {
+        GameObject apple = Instantiate(applePrefab);
+        apple.transform.position = this.transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+
+    // ѕерем≥щаЇмо €блуню
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime; //прив≥€зу до часу, а не до процесора FPS.
+                                         //Time.deltaTime - дл€ кожного ѕ  буде стала величина
+                                         //ѕерем≥щанн€ обЇкта буде однаковим дл€ ус≥х
+        transform.position = pos;
+
+        if(pos.x< -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);//перем≥щенн€ в право
+        }
+        else if(pos.x > leftAndRightEdge) {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
     }
 }
